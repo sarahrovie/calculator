4d2eeddb4812617d02dcd97fcd90f43da542edaf
Sarah <sarahrdvieira@gmail.com>
2025-03-14 19:29:31 -0300

    Add basic operator function with all operations

--- main.js
+++ main.js
@@ -6,3 +6,18 @@ function multiply(a, b) { return a * b; }
 
 function divide(a, b) { return a / b; }
 
+let num1;
+let num2;
+let operator;
+
+function operate(num1, operator, num2) {
+    if (operator === '+') {
+        return add(num1, num2);
+    } else if (operator === '-') {
+        return subtract(num1, num2);
+    } else if (operator === '*') {
+        return multiply(num1, num2);
+    } else if (operator === '/') {
+        return divide(num1, num2);
+    }
+}
