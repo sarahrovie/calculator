27bbfe9fc18e2bce9cc923fe4d1aa7711e504cfa
Sarah <sarahrdvieira@gmail.com>
2025-03-20 01:39:11 -0300

    Add getValues function to store all numbers for operate

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 const btns = document.querySelectorAll('.btn');
 let display = document.querySelector('#display');
 
+let values = [];
 let num1 = '';
 let num2 = '';
 let operator = '';
-const operators =  ['+', '-', '*', '/', '='];
+const operators =  ['+', '-', '*', '/'];
 
 function add(a, b) { return a + b; }
 
@@ -13,6 +14,28 @@ function subtract(a, b) { return a - b; }
 function multiply(a, b) { return a * b; }
 
 function divide(a, b) { return a / b; }
+
+function getValues(val) {
+    return values.push(val)
+}
+
+function addToDisplay(btn) {
+    let value = btn.id 
+
+    if (value === 'clear') {
+        display.textContent = ''
+    } else {
+        display.textContent += value
+        getValues(value)
+    }
+    console.log(values)
+}
+
+btns.forEach((btn) => {
+    btn.addEventListener('click', () => {
+        addToDisplay(btn)
+    })
+})
 
 function operate(num1, operator, num2) {
     if (operator === '+') {
@@ -25,17 +48,3 @@ function operate(num1, operator, num2) {
         return divide(num1, num2);
     }
 }
-
-function addToDisplay(btn) {
-    value = btn.id 
-
-    if (value === 'clear') {
-        display.textContent = ''
-    } else {
-        display.textContent += value
-    }
-}
-
-btns.forEach((btn) => {
-    btn.addEventListener('click', () => addToDisplay(btn))
-})
